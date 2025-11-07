f1f657025d61f222021405d84435def52e8f08cd
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-11-06 23:33:14 -0300

    Update listalinear.c

--- listalinear.c
+++ listalinear.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<conio.h> //usar o gotoxy
-#include<windows.h> //usar o gotoxy
-#include<locale.h>
+#include <locale.h>
+
 #define InicioArranjo 1
 #define MaxTam 5
 
 typedef int Apontador;
 
-typedef struct
-{
+typedef struct {
     int cod;
     int idade;
 } TipoItem;
 
-typedef struct
-{
+typedef struct {
     TipoItem Item[MaxTam];
     Apontador Primeiro;
     Apontador Ultimo;
@@ -25,177 +22,177 @@ TipoLista Lista;
 TipoItem x;
 Apontador pos;
 
-/*void gotoxy(int x, int y){ //Fun��o para centralizar
+/*
+void gotoxy(int x, int y){ //Função para centralizar
      SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){x-1,y-1});
-}*/ //para usar fun��o gotoxy
+}
+*/
 
+//---------------------------------------------
+// FUNÇÕES DE LISTA
+//---------------------------------------------
 
-void FLVazia (TipoLista *Lista)
-{
-    Lista->Primeiro=InicioArranjo;
-    Lista->Ultimo=Lista->Primeiro;
+void FLVazia(TipoLista *Lista) {
+    Lista->Primeiro = InicioArranjo;
+    Lista->Ultimo = Lista->Primeiro;
 }
 
-int Vazia (TipoLista Lista)
-{
-    return (Lista.Primeiro==Lista.Ultimo);
+int Vazia(TipoLista Lista) {
+    return (Lista.Primeiro == Lista.Ultimo);
 }
 
-void Insere(TipoItem x, TipoLista *Lista)
-{
-    if (Lista->Ultimo > MaxTam)
-        printf("\n A lista esta cheia\n");
-    else
-    {
-        Lista->Item[Lista->Ultimo -1]=x;
-        Lista->Ultimo=Lista->Ultimo+1;
+void Insere(TipoItem x, TipoLista *Lista) {
+    if (Lista->Ultimo - 1 >= MaxTam) {
+        printf("\nA lista está cheia!\n");
+    } else {
+        Lista->Item[Lista->Ultimo - 1] = x;
+        Lista->Ultimo = Lista->Ultimo + 1;
     }
-}	Continua��o de estrutura de dados abstratas - Lista por arranjo
+}
 
-void Imprime (TipoLista Lista)
-{
+void Imprime(TipoLista Lista) {
+    if (Vazia(Lista)) {
+        printf("\nA lista está vazia!\n");
+        return;
+    }
+
     int aux;
-    for (aux=Lista.Primeiro-1;aux<=(Lista.Ultimo-2);aux=aux+1)
-    {
-        printf("Cod: %d\n Idade: %d\n\n\n", Lista.Item[aux].cod, Lista.Item[aux].idade);
+    for (aux = Lista.Primeiro - 1; aux <= (Lista.Ultimo - 2); aux++) {
+        printf("Código: %d\nIdade: %d\n\n", Lista.Item[aux].cod, Lista.Item[aux].idade);
     }
 }
 
-void Retira (Apontador pos, TipoLista *Lista, TipoItem *x)
-{
+void Retira(Apontador pos, TipoLista *Lista, TipoItem *x) {
     int Aux;
-    if (Vazia (*Lista) || pos>=Lista->Ultimo)
-    {
-        printf("ERRO: Posi��o n�o existe\n");
-    }
-    else
-    {
-        *x = Lista->Item[pos-1];
-        printf("O elemento retirado foi o de codigo %d\n", x->cod);
-        Lista->Ultimo=Lista->Ultimo-1;
-        for (Aux =pos; Aux < Lista->Ultimo;Aux=Aux+1)
-            Lista->Item[Aux-1]=Lista->Item[Aux];
+
+    if (Vazia(*Lista) || pos < Lista->Primeiro || pos >= Lista->Ultimo) {
+        printf("ERRO: Posição inválida!\n");
+    } else {
+        *x = Lista->Item[pos - 1];
+        printf("O elemento retirado foi o de código %d\n", x->cod);
+
+        for (Aux = pos; Aux < Lista->Ultimo - 1; Aux++) {
+            Lista->Item[Aux - 1] = Lista->Item[Aux];
+        }
+
+        Lista->Ultimo = Lista->Ultimo - 1;
     }
 }
 
-void RetiraPos (Apontador pos, TipoLista *Lista, TipoItem *x)
-{
+void RetiraPos(Apontador pos, TipoLista *Lista, TipoItem *x) {
     int Aux;
-    if (Vazia (*Lista) || pos>=Lista->Ultimo-1)
-    {
-        printf("ERRO: Posi��o n�o existe\n");
-    }
-    else
-    {
+
+    if (Vazia(*Lista) || pos < Lista->Primeiro - 1 || pos > Lista->Ultimo - 2) {
+        printf("ERRO: Posição inválida!\n");
+    } else {
         *x = Lista->Item[pos];
-        printf("O elemento retirado foi o de codigo %d\n", x->cod);
-        Lista->Ultimo=Lista->Ultimo-1;
-        for (Aux =pos; Aux < Lista->Ultimo-1;Aux=Aux+1)
-            Lista->Item[Aux]=Lista->Item[Aux+1];
+        printf("O elemento retirado foi o de código %d\n", x->cod);
+
+        for (Aux = pos; Aux < Lista->Ultimo - 2; Aux++) {
+            Lista->Item[Aux] = Lista->Item[Aux + 1];
+        }
+
+        Lista->Ultimo = Lista->Ultimo - 1;
     }
 }
 
-void Procura(TipoItem x, TipoLista Lista)
-{
-    int i, encontrou=0;
-    for (i=Lista.Primeiro-1; i<=Lista.Ultimo-2;i=i+1)
-    {
-      if (Lista.Item[i].cod==x.cod)
-      {
-          encontrou=1;
-          printf("Codigo encontrado na posicao:%d\n",i);
-          break;
-      }
+void Procura(TipoItem x, TipoLista Lista) {
+    int i, encontrou = 0;
+
+    for (i = Lista.Primeiro - 1; i <= Lista.Ultimo - 2; i++) {
+        if (Lista.Item[i].cod == x.cod) {
+            encontrou = 1;
+            printf("Código encontrado na posição: %d\n", i + 1);
+            break;
+        }
     }
-    if (encontrou==0)
-    {
-        printf("Codigo nao encontrado\n");
+
+    if (!encontrou) {
+        printf("Código não encontrado!\n");
     }
 }
 
-int main ()
-{
+//---------------------------------------------
+// PROGRAMA PRINCIPAL
+//---------------------------------------------
+
+int main() {
     int opcao, resposta;
-    setlocale(LC_ALL, ""); //aceitar acentos
-    FLVazia(&Lista);
-    do
-    {
+    setlocale(LC_ALL, "");
 
-        printf("\n\nMENU\n\n");
-        printf("Digite a opc�o desejada:\n ");
+    FLVazia(&Lista);
+
+    do {
+        printf("\n\n====== MENU ======\n");
         printf("0 - Sair\n");
         printf("1 - Esvaziar lista\n");
-        printf("2 - Verificar se a lista esta vazia\n");
+        printf("2 - Verificar se a lista está vazia\n");
         printf("3 - Inserir elemento na lista\n");
         printf("4 - Imprimir os elementos da lista\n");
-        printf("5 - Retirar elemento da lista antes da posi��o determinada\n");
-        printf("6 - Retirar elemento da lista\n");
+        printf("5 - Retirar elemento (antes da posição determinada)\n");
+        printf("6 - Retirar elemento (na posição)\n");
         printf("7 - Pesquisar elemento na lista\n");
+        printf("==================\n");
+        printf("Digite a opção desejada: ");
         scanf("%d", &opcao);
-        switch (opcao)
-        {
+
+        switch (opcao) {
             case 0:
-            {
                 printf("PROGRAMA ENCERRADO!\n");
                 break;
-            }
+
             case 1:
-            {
                 FLVazia(&Lista);
-                printf("\nA LISTA VAZIA\n");
+                printf("\nA lista foi esvaziada.\n");
                 break;
-            }
+
             case 2:
-            {
-                resposta=Vazia(Lista);
-                if (resposta==1)
-                    printf("\n Lista esta vazia\n");
+                resposta = Vazia(Lista);
+                if (resposta == 1)
+                    printf("\nA lista está vazia.\n");
                 else
-                    printf("\n Lista n�o esta vazia\n");
+                    printf("\nA lista NÃO está vazia.\n");
                 break;
-            }
+
             case 3:
-            {
-                printf("Digite o codigo\n");
+                printf("Digite o código: ");
                 scanf("%d", &x.cod);
-                printf("Digite a idade\n");
+                printf("Digite a idade: ");
                 scanf("%d", &x.idade);
                 Insere(x, &Lista);
                 break;
-            }
+
             case 4:
-            {
                 Imprime(Lista);
                 break;
-            }
+
             case 5:
-            {
-                printf("Digite a posi��o\n");
+                printf("Digite a posição: ");
                 scanf("%d", &pos);
                 Retira(pos, &Lista, &x);
                 break;
-            }
+
             case 6:
-            {
-                printf("Digite a posi��o\n");
+                printf("Digite a posição: ");
                 scanf("%d", &pos);
                 RetiraPos(pos, &Lista, &x);
                 break;
-            }
+
             case 7:
-            {
-                printf("Digite o elemento\n");
+                printf("Digite o código a pesquisar: ");
                 scanf("%d", &x.cod);
                 Procura(x, Lista);
                 break;
-            }
 
-            default: printf("\n\nOpcao invalida\n\n");
-        } //fim do case
-        _getch();
+            default:
+                printf("\nOpção inválida!\n");
+        }
+
+        printf("\nPressione qualquer tecla para continuar...");
+        system("pause");
         system("cls");
-    } while (opcao != 0);//fim do while
-    return(0);
+
+    } while (opcao != 0);
+
+    return 0;
 }
-
-
